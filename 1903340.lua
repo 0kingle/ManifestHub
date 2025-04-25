032842581c8370e7ff5dd334b4e39cf876eb9ecb
0kingle <gabriellopeskingle@gmail.com>
2025-04-24 23:00:43 -0300

    Update and rename 1903340.lua to 2492040

--- 1903340.lua
+++
@@ -1,5 +0,0 @@
--- manifest & lua provided by: https://www.piracybound.com/discord
--- via manilua
-addappid(1903340)
-addappid(1903341, 1, "8593f9c4cd595f833d5344e55cd6c6fdb73d9a4fa05dab033ba71622265e8c82")
-setManifestid(1903341, "213847257621599287", 0)
